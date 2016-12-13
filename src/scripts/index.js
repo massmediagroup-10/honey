83312bdb872bfe340a3052e8c5d4d4c084ad1775
node-ist <node_ist@massmediagroup.pro>
2016-12-13 12:05:54 +0200

    product page done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,6 +9,59 @@ function initSlider() {
         prevArrow: '<a href="#" class="slick-prev"><i class="fa fa-chevron-left"></i></a>',
         nextArrow: '<a href="#" class="slick-next"><i class="fa fa-chevron-right"></i></a>'
     });
+
+    $('.detail-slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '.detail-slider-nav',
+        prevArrow: '<a href="#" class="slick-prev"><i class="fa fa-chevron-left"></i></a>',
+        nextArrow: '<a href="#" class="slick-next"><i class="fa fa-chevron-right"></i></a>'
+    });
+    $('.detail-slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.detail-slider-for',
+        focusOnSelect: true,
+        arrows: false,
+        vertical: true,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 6,
+                vertical: false
+            }
+        }, {
+            breakpoint: 780,
+            settings: {
+                slidesToShow: 4,
+                vertical: false
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3,
+                vertical: false
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                vertical: false
+            }
+        }]
+    });
+}
+
+function detailToggle() {
+    $('.tabs-item-inner').slideUp(0);
+    $(document).on('click', '.tabs-item h5', function() {
+        if ($(this).siblings('.tabs-item-inner').length) {
+            $(this).stop().toggleClass('active');
+            $(this).siblings('.tabs-item-inner').stop().slideToggle();
+        }
+    });
 }
 
 function foundation() {
@@ -18,4 +71,5 @@ function foundation() {
 $(document).ready(function() {
     initSlider();
     foundation();
+    detailToggle();
 });
